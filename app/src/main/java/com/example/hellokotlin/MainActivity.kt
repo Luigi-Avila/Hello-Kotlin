790d0a9647f05f2afdd96e9b4a5bb3a29e42b954
Luigi-Avila <l.avila.uvg@gmail.com>
2023-03-01 17:48:21 -0600

    feat: text to speech configuration added

--- app/src/main/java/com/example/hellokotlin/MainActivity.kt
+++ app/src/main/java/com/example/hellokotlin/MainActivity.kt
@@ -2,11 +2,26 @@ package com.example.hellokotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener  {
+
+    var tts: TextToSpeech? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        tts = TextToSpeech(this, this)
+
+    }
+
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS){
+            findViewById<TextView>(R.id.textView).text = "Listo!"
+        } else {
+            findViewById<TextView>(R.id.textView).text = "No disponible :("
+        }
     }
 }
